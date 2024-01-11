b172ceb5a33752e3d6f1e151081a039386225494
Renny <rennyemmanuel8@gmail.com>
2024-01-11 08:32:08 -1200

    display: board

---
+++ tic-tac-toe.py
@@ -0,0 +1,18 @@
+# printing the game board
+board = ['_', '_', '_',
+         '_', '_', '_',
+         '_', '_', '_']
+
+def display_board(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+display_board(board)
+
+# take player input
+
+# check for win or tie
+
+# switch the player
+
+# check for win or tie
