049b1af87e5ffb998bf5c94090baaed40ea8471d
Renny <rennyemmanuel8@gmail.com>
2024-01-11 08:56:46 -1200

    added: player input

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,15 +1,28 @@
-# printing the game board
+# Creating the game board
 board = ['-', '-', '-',
          '-', '-', '-',
          '-', '-', '-']
 
+# Displaying the game board
 def display_board(board):
     print(board[0] + " | " + board[1] + " | " + board[2])
     print("----------")
     print(board[3] + " | " + board[4] + " | " + board[5])
     print("----------")
     print(board[6] + " | " + board[7] + " | " + board[8])
-display_board(board)
+
+# Getting player input
+current_player = "X"
+def get_player_input(board):
+    usr_inp = int(input("Choose a board space 1-9: "))
+    if usr_inp >= 1 and usr_inp <= 9 and board[usr_inp-1] == "-":
+        board[usr_inp-1] = current_player
+    else:
+        print("Oh noo, that spot is already taking(*_*)")
+
+while True:
+    display_board(board)
+    get_player_input(board)
 
 # take player input
 
