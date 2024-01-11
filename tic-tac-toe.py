dc35bbe4d670d5aeb02f8fb330e2d626931ea7b8
Renny <rennyemmanuel8@gmail.com>
2024-01-11 08:38:01 -1200

    modify: board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,11 +1,13 @@
 # printing the game board
-board = ['_', '_', '_',
-         '_', '_', '_',
-         '_', '_', '_']
+board = ['-', '-', '-',
+         '-', '-', '-',
+         '-', '-', '-']
 
 def display_board(board):
     print(board[0] + " | " + board[1] + " | " + board[2])
+    print("----------")
     print(board[3] + " | " + board[4] + " | " + board[5])
+    print("----------")
     print(board[6] + " | " + board[7] + " | " + board[8])
 display_board(board)
 
